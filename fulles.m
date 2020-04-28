d602573327f87fdb85f97f27b6139783a6c9d61a
Dvyd <dvyd98@gmail.com>
2020-04-28 02:07:03 +0200

    ho he fet amb 2000 descriptros, segurament m'he passat :) accuracy del 73%

--- fulles.m
+++ fulles.m
@@ -1,20 +1,14 @@
 function [outputArg1] = fulles(inputArg1)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-im=imread(inputArg1);
-im = imresize(im,[256 256]);
-%im=imresize(im,1/4);
-%imshow(im), title('imatge original');
-bw = im2bw(im,0.6);
+
+bw = im2bw(inputArg1,0.6);
 bw = ~bw;
-%imshow(bw), title('imatge bw');
 ee = strel('disk',5);
 bw = imclose(bw,ee);
-%imshow(bw), title('imatge bw closed');
 
 ero=imerode(bw,strel('disk',1));
 cont=xor(ero,bw);
-%figure,imshow(cont), title('contorns');
 
 % obtenim les coordenades del contorn
 [fila col] = find(bw,1); % Busquem el primer píxel
@@ -23,7 +17,6 @@ B = bwtraceboundary(bw,[fila col],'E'); %direccio est a l'atzar
 % Ho comprovem mostrant el resultat
 aux=zeros(size(bw));
 aux(sub2ind(size(aux),B(:,1),B(:,2)))=1;
-%figure,imshow(aux),title ('contorns a partir de coordenades');
 
 % centrem coordenades
 mig=mean(B);
@@ -39,28 +32,7 @@ if(mida/2~=round(mida/2))
 end
 % Calculem la Fast Fourier Transform
 z=fft(s);
-% representem l’espectre
-%figure,plot(abs(z))
-% ho displaiem logaritmic perque no es veu res
-%figure,plot(log(abs(z))), title ('descriptors de Fourier');
 
-% Recuperem la imatge original per comprovar que el procés és reversible
-ss=ifft(z); % Transformada de Fourier inversa
-files= round(real(ss)+mig(1));
-cols=round(imag(ss)+mig(2));
-aux(:,:)=0;
-aux(sub2ind(size(aux),files,cols))=1;
-%figure,imshow(aux), title('imatge recuperada');
-
-% Reduim la quantitat de descriptors de Fourier
-N=100; % agafem N descriptors
-tmp=z;
-tmp(N+1:end-N)=0; % eliminem els del mig perque es duplica l'espectre
-%figure, plot(log(abs(tmp))), title ('Només 30 descriptors')
-% Tornem al pla imatge a partir de l’espectre modificat
-ss2=ifft(tmp);
-
-
-outputArg1 = ss2;
+outputArg1 = z;
 end
 
